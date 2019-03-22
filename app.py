a8d992b994e0234b86d4eaba7a5d06bc244c5a9c
mnbn <martin.blomgren@starstable.com>
2019-03-22 11:26:58 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_index():
+    return "Fetch Ops users: 127.0.0.1:5000/user/"
+
+@app.route('/user/<user>')
+def get_user(user):
+    r = requests.get('http://crud:5000/user/' + user)
+    return "Fetched through the ops_client: " + r.text
