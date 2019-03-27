a38143a1bb4babd698c899a9b1c616efe76512d3
mnbn <martin.blomgren@starstable.com>
2019-03-27 16:26:22 +0100

    Adds healthcheck /ping

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main_index():
     return "Ops client: 127.0.0.1:5000/user/"
 
+@app.route('/ping')
+def ping():
+    return ""
+
 @app.route('/user/<user>')
 def get_user(user):
     r = requests.get('http://crud:5000/user/' + user)
