e5ee8d3fdb6dbccbeedf0c26c4bcac9f899b2f44
mnbn <martin.blomgren@starstable.com>
2019-03-28 13:26:38 +0100

    Adds healthcheck /ping

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect
+from healthcheck import HealthCheck, EnvironmentDump
 import requests
 
 app = Flask(__name__)
+
+# wrap the flask app and give a heathcheck url
+health = HealthCheck(app, "/healthcheck")
+envdump = EnvironmentDump(app, "/environment")
 
 @app.route('/')
 def main_index():
     return "Ops client: 127.0.0.1:5000/user/"
 
-@app.route('/ping')
-def ping():
-    return ""
-
 @app.route('/user/<user>')
 def get_user(user):
     r = requests.get('http://crud:5000/user/' + user)
     return "Fetched through the ops_client: " + r.text
+
+@app.route('/ping')
+def ping():
+    return redirect('/healthcheck')
