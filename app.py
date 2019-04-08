a190b325946af798af61230ffc92964864a94260
mnbn <martin.blomgren@starstable.com>
2019-04-08 16:24:46 +0200

    Working with path routing in ALB

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ from healthcheck import HealthCheck, EnvironmentDump
 import requests
 import os
 
+alb = os.getenv('ALB', 'localhost')
+service_name = os.getenv('SERVICE_NAME', '')
+
+prefix = ''
+if service_name:
+    prefix = '/' + service_name
+
 app = Flask(__name__)
 
 # wrap the flask app and give a heathcheck url
-health = HealthCheck(app, "/healthcheck")
-envdump = EnvironmentDump(app, "/environment")
+health = HealthCheck(app, prefix + "/healthcheck")
+envdump = EnvironmentDump(app, prefix + "/environment")
 
 appenv = os.getenv('APP_ENV', '.staging.fargate.local')
 
-@app.route('/')
+@app.route(prefix + '/')
 def main_index():
     return 'Ops client: APP_ENV: ' + appenv
 
-@app.route('/user/<user>')
+@app.route(prefix + '/user/<user>')
 def get_user(user):
-    r = requests.get('http://crud' + appenv + ':5000/user/' + user)
+    r = requests.get('http://' + alb + '/crud/user/' + user)
     return "Fetched through the ops_client: " + r.text
-
-@app.route('/ping')
-def ping():
-    return redirect('/healthcheck')
