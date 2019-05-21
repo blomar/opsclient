95a44fc1a206e4abc9f2cf65865716ab9a6f785d
mnbn <martin.blomgren@starstable.com>
2019-05-21 16:11:03 +0200

    Adds logging

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from aws_xray_sdk.core import patch_all
 
+import logging
+import sys
 import requests
 import os
 
@@ -11,26 +13,36 @@ alb = os.getenv('ALB', 'localhost')
 service_name = os.getenv('SERVICE_NAME', '-')
 service_version = os.getenv('SERVICE_VERSION', '-')
 service_environment = os.getenv('SERVICE_ENVIRONMENT', '-')
+prefix = ('/%s' % service_name)
 
-prefix = ''
-if service_name:
-    prefix = '/' + service_name
+#Logging
+stdout_handler = logging.StreamHandler(sys.stdout)
+handlers = [stdout_handler]
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(lineno)d:%(name)-8s %(message)s',
+    handlers=handlers
+)
+LOGGER = logging.getLogger(__name__)
 
 app = Flask(__name__)
+LOGGER.info('start application - %s', service_name)
 
 xray_recorder.configure(service=service_name)
 XRayMiddleware(app, xray_recorder)
 patch_all()
 
 # wrap the flask app and give a heathcheck url
-health = HealthCheck(app, prefix + "/admin/healthcheck")
-envdump = EnvironmentDump(app, prefix + "/admin/environment")
+health = HealthCheck(app, prefix + '/admin/healthcheck')
+envdump = EnvironmentDump(app, prefix + '/admin/environment')
 
 @app.route(prefix + '/')
 def main_index():
+    LOGGER.info('main_index')
     return 'NAME: %s\nVERSION: %s\nENVIRONMENT: %s\n' % (service_name, service_version, service_environment)
 
 @app.route(prefix + '/user/<user>')
 def get_user(user):
+    LOGGER.info('Try to fetch user: %s', user)
     r = requests.get('http://' + alb + '/crud/user/' + user)
-    return "Fetched through the ops/client: " + r.text
+    return 'Fetched through the ops/client: ' + r.text
