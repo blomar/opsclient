7ded4ae0a1666c1fd62b113ba63a4dd8888f4986
mnbn <martin.blomgren@starstable.com>
2019-09-18 17:00:53 +0200

    Lint fixes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ service_version = os.getenv('SERVICE_VERSION', '-')
 service_environment = os.getenv('SERVICE_ENVIRONMENT', '-')
 prefix = ('/%s' % service_name)
 
-#Logging
+# Logging
 json_logging.ENABLE_JSON_LOGGING = True
 json_logging.init(framework_name='flask')
 stdout_handler = logging.StreamHandler(sys.stdout)
@@ -36,23 +36,57 @@ xray_recorder.configure(service=service_name)
 XRayMiddleware(app, xray_recorder)
 patch_all()
 
-# wrap the flask app and give a heathcheck url
+# wrap the flask app and give a healthcheck url
 health = HealthCheck(app, prefix + '/admin/healthcheck')
 envdump = EnvironmentDump(app, prefix + '/admin/environment')
 
+
 @app.route(prefix + '/')
 def main_index():
-    LOGGER.info('main_index', extra = {'props' : {'name' : service_name, 'version' : service_version, 'environment' : service_environment}})
+    LOGGER.info('main_index',
+                extra={'props': {'name': service_name, 'version': service_version, 'environment': service_environment}})
     return 'NAME: %s\nVERSION: %s\nENVIRONMENT: %s\n' % (service_name, service_version, service_environment)
 
-@app.route(prefix + '/user/<user>')
-def get_user(user):
-    LOGGER.info('Try to fetch user: %s', user, extra = {'props' : {'user' : user}})
+
+@app.route(prefix + '/v1/user/<user>')
+def get_user_v1(user):
+    LOGGER.info('Try http://%s/crud/user/%s', alb, user, extra={'props': {'user': user}})
     response = requests.get('http://' + alb + '/crud/user/' + user)
     return 'Fetched through the ops/client: ' + response.text
 
-@app.route(prefix + '/random')
-def get_random():
-    response = requests.get('http://' + alb + '/crud/stats')
-    LOGGER.info('Radom, code %s', response.status_code)
-    return response.text, response.status_code
+
+@app.route(prefix + '/v2/user/<user>')
+def get_user_v2(user):
+    LOGGER.info('USER')
+    conn_url = 'http://crud.' + service_environment + '.local:5000/crud/user/' + user
+    LOGGER.info('Try %s', conn_url, extra={'props': {'user': user}})
+
+    try:
+        response = requests.get(conn_url)
+
+        # Consider any status other than 2xx an error
+        if not response.status_code // 100 == 2:
+            return "Error: Unexpected response {}".format(response)
+
+        return 'Fetched through the ops/client: ' + response.text
+    except requests.exceptions.RequestException as e:
+        # A serious problem happened, like an SSLError or InvalidURL
+        return "Error: {}".format(e)
+
+
+@app.route(prefix + '/test')
+def get_test():
+    LOGGER.info('TEST')
+    conn_url = 'http://crud.' + service_environment + '.local:5000/crud/'
+    LOGGER.info('Try %s', conn_url)
+    try:
+        response = requests.get(conn_url)
+
+        # Consider any status other than 2xx an error
+        if not response.status_code // 100 == 2:
+            return "Error: Unexpected response {}".format(response)
+
+        return 'Fetched through the ops/client: ' + response.text
+    except requests.exceptions.RequestException as e:
+        # A serious problem happened, like an SSLError or InvalidURL
+        return "Error: {}".format(e)
